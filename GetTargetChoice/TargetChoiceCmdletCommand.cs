8144ce229ef1d4c4a6d9407dd600d54f868d11d5
Yoann BLOSSIER <yoann.blossier@toenn-vaot.fr>
2024-02-21 06:25:09 +0100

    Initiate

---
+++ GetTargetChoice/TargetChoiceCmdletCommand.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.ObjectModel;
+using System.Management.Automation;
+using System.Management.Automation.Host;
+
+namespace GetTargetChoice
+{
+    /// <summary>
+    /// <para type="synopsis">Function to ask question to user to choose between Debug or Release target</para>
+    /// <para type="description">This function ask question to the user and wait the choice of the user between '0' (Debug) and '1' (Release).</para>
+    /// <para type="description">When the user choices a right option, the function returns the target string resulting : Debug if it is '0', Release if it is '1'</para>
+    /// <example>
+    /// <para>Usage</para>
+    /// <para><code>Get-TargetChoice("What the target [0-Debug] / [1-Release] ?")</code></para>
+    /// </example>
+    /// </summary>
+    /// <para type="link" uri="https://github.com/toenn-vaot/psmodule-get-target-choice">Source code</para>
+    [Cmdlet(VerbsCommon.Get, "TargetChoice")]
+    [OutputType(typeof(string))]
+    public class TargetChoiceCmdletCommand : PSCmdlet
+    {
+        /// <summary>
+        /// <para type="description">The question asked user</para>
+        /// </summary>
+        [Parameter(Mandatory = true, HelpMessage = "What is the question you want to ask ?", ValueFromPipeline = true)]
+        [Alias("q")]
+        public string Question { get; set; }
+        
+        /// <inheritdoc />
+        protected override void ProcessRecord()
+        {
+            var targets = new Collection<string> { "Debug", "Release" };
+            var debug = new ChoiceDescription("&Debug", "Continue in DEBUG mode");
+            var release = new ChoiceDescription("&Release", "Continue in RELEASE mode");
+            var options = new Collection<ChoiceDescription>{ debug, release };
+
+            var choice = Host.UI.PromptForChoice("", Question, options, 0);
+            WriteVerbose($"The target choice is {choice} corresponding to {targets[choice]}");
+
+            WriteObject(targets[choice]);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-# psmodule-get-target-choice
+# GetBoolChoice
+
+[![PowerShell Gallery - PowerShellGet](https://img.shields.io/badge/PowerShell%20Gallery-PowerShellGet-blue.svg)](https://www.powershellgallery.com/packages/GetTargetChoice)
+[![Minimum Supported PowerShell Version](https://img.shields.io/badge/PowerShell-5.1-blue.svg)](https://github.com/Toenn-Vaot/psmodule-get-GetTargetChoice-choice)
+
+Introduction
+============
+GetBoolChoice is a module that allows use Get-TargetChoice cmdlet which ask question to get the process target choosed by the user between Debug and Release.
+
+Documentation
+=============
+
+```powershell
+Get-Help GetTargetChoice
+```
+
+Requirements
+============
+
+- Windows PowerShell 5.1 or newer.
+
+Get PowerShellGet Module
+========================
+
+Please refer to our [documentation](https://www.powershellgallery.com/packages/GetTargetChoice/) for the up-to-date version on how to get the GetTargetChoice Module.
+
+Get PowerShellGet Source
+========================
+
+#### Steps
+* Obtain the source
+    - Clone the repository (needs git)
+    ```powershell
+    git clone https://github.com/Toenn-Vaot/psmodule-get-target-choice
+    ```
